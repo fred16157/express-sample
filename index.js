f38095645dee588a77afe70c7b581d70577e70dc
강현구 <fred16157@gmail.com>
2021-02-09 18:47:17 +0900

    프로젝트 파일 추가

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')();
+var bodyParser = require('body-parser');
+express.set('trust proxy', true);
+express.use(bodyParser.json());
+express.use(bodyParser.urlencoded({ extended: false}));
+
+express.set('views', path.join(__dirname, 'views'));
+express.set('view engine', 'ejs');
+
+express.get('/', (req, res) => {
+    res.render('index');
+});
